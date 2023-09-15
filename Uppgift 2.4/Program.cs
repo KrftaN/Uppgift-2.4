ec9fc094bedc5eaf6dc5f6f84bf69a8367480065
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-15 12:53:17 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket tjänar anställd nummer 1?");
+            int lön1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar anställd nummer 2?");
+            int lön2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar anställd nummer 3?");
+            int lön3 = Convert.ToInt32(Console.ReadLine());
+
+            int summa = (lön1 + lön2 + lön3) / 3;
+
+            Console.WriteLine("Medellönen är: " + summa);
+            Console.ReadKey();
+        }
+    }
+}
